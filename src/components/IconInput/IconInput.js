57660cb80aa9f6ed9b55fe1d68b30455d308fe40
AnnaIgnateva12 <anna.ignateva@simbirsoft.com>
2024-12-17 10:37:49 +0400

    IconInput

--- src/components/IconInput/IconInput.js
+++ src/components/IconInput/IconInput.js
@@ -6,6 +6,41 @@ import { COLORS } from '../../constants';
 import Icon from '../Icon';
 import VisuallyHidden from '../VisuallyHidden';
 
+const Wrapper = styled.div`
+ position: relative;
+ border-bottom: 1px solid ${COLORS.black};
+ overflow: hidden;
+ width: fit-content;
+ color: ${COLORS.gray700};
+  &:hover {
+    color: ${COLORS.black};
+  }
+`
+const StyledInput = styled.input`
+  position: relative;
+  bottom: 6px;
+  margin-left: 8px;
+  border: none;
+  outline: none;
+  font-size: var(--fontSize);
+  font-weight: 700;
+  color: inherit;
+  width: var(--width);
+  &::placeholder {
+    color: ${COLORS.gray500};
+    font-weight: 400;
+  }
+`
+const SIZES = {
+  small: {
+    fontSize: 14,
+    iconSize: 24
+  },
+  large: {
+    fontSize: 18,
+    iconSize: 36
+  }
+}
 const IconInput = ({
   label,
   icon,
@@ -13,7 +48,15 @@ const IconInput = ({
   size,
   placeholder,
 }) => {
-  return 'TODO';
-};
+  const styles = SIZES[size];
+  return (
+      <Wrapper style={{'--width': `${width}px`, '--fontSize': `${styles.fontSize}px`}}>
+        <label htmlFor={label}>
+          <Icon id={icon} display='inline' size={styles.iconSize}/>      
+          <StyledInput width={width} placeholder={placeholder} id={label}/>
+        </label>
+      </Wrapper>
+    
+)};
 
 export default IconInput;
